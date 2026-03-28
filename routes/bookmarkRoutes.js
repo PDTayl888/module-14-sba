d89e291d56cefd97b47ccc03f7d16adb61914c5d
Patrick D. Taylor <patrickdtaylor1980@gmail.com>
2026-03-28 18:10:17 -0400

    edited bookmarkRoutes

--- routes/bookmarkRoutes.js
+++ routes/bookmarkRoutes.js
@@ -7,18 +7,52 @@ const router = express.Router();
 router.use(authMiddleware);
 
 router.post('/', async (req, res) => {
+    try {
+        
+    } catch (error) {
+        
+    }
+}
 
 
 router.get('/', async (req, res) => {
+    try {
+        
+    } catch (error) {
+        
+    }
+
+}
 
 
     router.get('/:id', async (req, res) => {
+    try {
+        
+    } catch (error) {
+        
+    }
+
+    }
 
 
         router.put('/:id', async (req, res) => {
+    try {
+        
+    } catch (error) {
+        
+    }
+
+        }
 
 
             router.delete('/:id', async (req, res) => {
+    try {
+        
+    } catch (error) {
+        
+    }
+
+            }
 
 
                 module.exports = router;
