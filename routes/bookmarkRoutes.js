0b064d577532fba593438c1a3074fafdb77ca3d7
Patrick D. Taylor <patrickdtaylor1980@gmail.com>
2026-03-28 18:07:59 -0400

    edited bookmarkRoutes

--- routes/bookmarkRoutes.js
+++ routes/bookmarkRoutes.js
@@ -5,3 +5,20 @@ const { authMiddleware } = require('../utils/auth');
 const router = express.Router();
 
 router.use(authMiddleware);
+
+router.post('/', async (req, res) => {
+
+
+router.get('/', async (req, res) => {
+
+
+    router.get('/:id', async (req, res) => {
+
+
+        router.put('/:id', async (req, res) => {
+
+
+            router.delete('/:id', async (req, res) => {
+
+
+                module.exports = router;
