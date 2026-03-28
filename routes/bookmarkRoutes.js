c2c28e11d3a2e498a812695bd8f4dee76e74a8e1
Patrick D. Taylor <patrickdtaylor1980@gmail.com>
2026-03-28 18:23:40 -0400

    edited bookmarkRoutes

--- routes/bookmarkRoutes.js
+++ routes/bookmarkRoutes.js
@@ -1,65 +1,61 @@
-const express = require('express');
-const Bookmark = require('../models/Bookmark');
-const { authMiddleware } = require('../utils/auth');
+const express = require("express");
+const Bookmark = require("../models/Bookmark");
+const { authMiddleware } = require("../utils/auth");
 
 const router = express.Router();
 
 router.use(authMiddleware);
 
-router.post('/', async (req, res) => {
-    const { title, url} = req.body;
-    try {
-        const bookmark = await Bookmark.create({
-            title, 
-            url,
-            user: req.user._id
-        });
-        res.status(201).json(bookmark);
-    } catch (error) {
-        res.status(500).json({ message: 'SERVER ERROR', error: error.message});
-    }
+router.post("/", async (req, res) => {
+  const { title, url } = req.body;
+  try {
+    const bookmark = await Bookmark.create({
+      title,
+      url,
+      user: req.user._id,
+    });
+    res.status(201).json(bookmark);
+  } catch (error) {
+    res.status(500).json({
+      message: "SERVER ERROR",
+      error: error.message,
+    });
+  }
 });
 
-
-router.get('/', async (req, res) => {
-    try {
-        const bookmarks = await Bookmark.find({ user: req.user._id });
-        res.json(bookmarks);
-    } catch (error) {
-          res.status(500).json({ message: 'SERVER ERROR', error: error.message});
-    }
-
+router.get("/", async (req, res) => {
+  try {
+    const bookmarks = await Bookmark.find({ user: req.user._id });
+    res.json(bookmarks);
+  } catch (error) {
+    res.status(500).json({ message: "SERVER ERROR", error: error.message });
+  }
 });
 
-
-    router.get('/:id', async (req, res) => {
-    try {
-        
-    } catch (error) {
-        
+router.get("/:id", async (req, res) => {
+  try {
+    const bookmark = await Bookmark.findById(req.params.id);
+    if (!bookmark) {
+      return res.status(404).json({ message: "BOOKMARD NOT FOUND" });
     }
 
+    if (bookmark.user.toString() !== req.user._id.toString()) {
+      return res.status(403).json({ message: "NOT AUTHORIZED" });
     }
+    res.status(bookmark);
+  } catch (error) {
+    res.status(500).json({ message: "SERVER ERROR", error: error.message });
+  }
+});
 
+router.put("/:id", async (req, res) => {
+  try {
+  } catch (error) {}
+});
 
-        router.put('/:id', async (req, res) => {
-    try {
-        
-    } catch (error) {
-        
-    }
+router.delete("/:id", async (req, res) => {
+  try {
+  } catch (error) {}
+});
 
-        }
-
-
-            router.delete('/:id', async (req, res) => {
-    try {
-        
-    } catch (error) {
-        
-    }
-
-            }
-
-
-                module.exports = router;
+module.exports = router;
